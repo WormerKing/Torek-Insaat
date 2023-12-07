b52a454e8d10a2c9001c3beb77f397ebe498e7bb
Tuncay Dinler <aferinsanaaferin@gmail.com>
2023-12-07 20:10:05 +0300

    install.sh güncellendi

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@ echo "Ubuntu kurulum işlemi başladı"
 sudo adduser wormer
 sudo usermod -a -G sudo wormer
 
-sudo -u wormer -s
-
 sudo apt update -y && sudo apt upgrade -y
 
-cd
+cd /home/wormer
 
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.3.3/fastfetch-2.3.3-Linux.deb
 sudo dpkg -i fastfetch-2.3.3-Linux.deb
