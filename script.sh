74055e7af934f77089b60fa103d6f170bfe27f1e
Tuncay Dinler <aferinsanaaferin@gmail.com>
2024-01-21 22:21:39 +0300

    Bazı hata düzeltmeleri eklendi

--- script.sh
+++ script.sh
@@ -3,6 +3,7 @@
 echo "Derleme işlemi başladı"
 
 # bin/rails db:migrate RAILS_ENV=development 
+bin/rails db:drop && bin/rails db:create && bin/rails db:migrate && bin/rails db:seed
 
 cp /etc/hosts /etc/hosts.new && \
 sed -i 's/::1\tlocalhost ip6-localhost ip6-loopback/::1 ip6-localhost ip6-loopback/' /etc/hosts.new && \
